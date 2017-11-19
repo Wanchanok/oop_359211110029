27c865dfecd1de7e778b4e9be65b5e59ba92da70
Wanchanok <audition_za5@hotmail.com>
2017-11-19 10:32:58 +0700

    Add Information

--- src/oop_lab/s359211110029/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110029/rmutsv/com/HelloWorld.java
@@ -7,8 +7,9 @@ public static void main(String[] args) {
 
     System.out.println("Hell world, I'm GBO");
     System.out.println("My name is Wanchaok lerstanakitpaisal");
-
-
+//    add infomation
+    System.out.println("Faculty: Management Technology");
+    System.out.println("RMUTSV");
 
 
 }// main
