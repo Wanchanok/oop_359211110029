5d6becbd9fbb78b6d41878bbab443bcc352a0a84
Wanchanok <audition_za5@hotmail.com>
2017-11-19 10:36:17 +0700

    Testing delete Information

--- src/oop_lab/s359211110029/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110029/rmutsv/com/HelloWorld.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
     System.out.println("Hell world, I'm GBO");
     System.out.println("My name is Wanchaok lerstanakitpaisal");
 //    add infomation
-    System.out.println("Faculty: Management Technology");
-    System.out.println("RMUTSV");
+    System.out.println("Faculty: Management Technology, RMUTSV");
+
 
 
 }// main
