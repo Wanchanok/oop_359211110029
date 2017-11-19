de687c8310a03505b44de0595b5813d7892709ec
Wanchanok <audition_za5@hotmail.com>
2017-11-19 09:48:59 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110029/rmutsv/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110029.rmutsv.com;
+
+public class HelloWorld
+{
+//   Create main method
+public static void main(String[] args) {
+
+    System.out.println("Hell world, I'm GBO");
+    System.out.println("My name is Wanchaok lerstanakitpaisal");
+
+
+
+
+}// main
+}// class
