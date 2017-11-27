8bd8394dd12aef25a0bcc3254d41dd9298b645e9
Wanchanok <leejeebo13@gmail.com>
2017-11-27 15:44:18 +0700

    CalGrade

--- src/ooplab3/CalGrade.java
+++ src/ooplab3/CalGrade.java
@@ -1,10 +1,34 @@
 package ooplab3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalGrade {
 //    score 0-49 grad F
 //    score 50-59 grad D
 //    score 60-69 grad C
 //    score 70-79 grad B
 //    score 80-100 grad A
-}
+public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter your score : ");
+    int score = Integer.parseInt(reader.readLine());
+    if (score<50){
+        System.out.println("Your grad  : F");
+    }
+    else if (score<=59){
+        System.out.println("Your grad  : D");
+    }
+    else if (score<=69){
+        System.out.println("Your grad : C");
+    }
+    else if (score<=79){
+        System.out.println("Your grad : B");
+    }
+    else{
+        System.out.println("Your grad A");
+    }
+}// main
+}//class
 
---
+++ src/ooplab3/TestSeitch.java
@@ -0,0 +1,37 @@
+package ooplab3;
+
+import java.util.Scanner;
+
+public class TestSeitch {
+    public static void main(String[] args) {
+        int select;
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter number between 1-3: ");
+//        select = scanner.nextInt();
+//
+//        // switch-case
+//        switch (select) {
+//            case 1:
+//                System.out.println("You enter 1");
+//                break;
+//            case 2:
+//                System.out.println("You enter 2");
+//                break;
+//            case 3:
+//                System.out.println("You enter 3");
+//                break;
+//            default:
+//                System.out.println("Please enter number brtween 1-3, Thank you");
+//      switch
+        char c = 'A';
+        switch (c){
+            case 'A' : System.out.println("A ant");
+            case 'B' : System.out.println("B bird");
+            case 'C' : System.out.println("C cat");
+            default : System.out.println("D dog");
+//        }
+
+        }//main
+
+    }//class
+}
