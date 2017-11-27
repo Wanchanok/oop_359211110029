a5894dfebb79e1b306a469e067807d2e001eb25d
Wanchanok <leejeebo13@gmail.com>
2017-11-27 16:38:52 +0700

    TestForLoop

--- src/ooplab3/TestForLoop.java
+++ src/ooplab3/TestForLoop.java
@@ -3,7 +3,7 @@ package ooplab3;
 public class TestForLoop {
     public static void main(String[] args) {
     for (int i=1;i<=100;i++) {
-        if (i % 3 ==0|| i % 5 == 0|| i % 7 ==0)
+        if (i % 2 ==0|| i % 4 == 0|| i % 6 ==0)
             System.out.print(" " + i);
 
     }//f
