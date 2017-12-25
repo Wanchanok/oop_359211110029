9ee72edc0e8995b946d128d9c0ebce13e80beec6
Wanchanok <leejeebo13@gmail.com>
2017-12-25 18:12:49 +0700

    myStrBuffer

--- src/ooplab6/myStrBuffer.java
+++ src/ooplab6/myStrBuffer.java
@@ -2,7 +2,21 @@ package ooplab6;
 
 public class myStrBuffer {
     public static void main(String[] args) {
-
+// StringBuffer
+        StringBuffer buffer =new StringBuffer();
+        buffer.append("Hello");
+        buffer.append(" ");
+        buffer.append("to");
+        buffer.append(" ");
+        buffer.append("RMUTSV");
+        buffer.append(" ");
+        buffer.append(2017);
+        System.out.println(buffer);
+        buffer.replace(0,5,"University of");
+        System.out.println(buffer);
+        System.out.println(buffer.reverse()); //สลับหน้ามาหลัง
+        buffer.setCharAt(0,'T');
+        System.out.println(buffer); //เปลี่ยนค่า ตำแหน่ง0 เป็น T
 
     }//main
 }//class
