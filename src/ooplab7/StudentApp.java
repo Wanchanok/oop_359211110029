38be492a219b6a721d4bf7659cfc8b86da204cc1
Wanchanok <leejeebo13@gmail.com>
2018-01-22 14:47:48 +0700

    Student input

--- src/ooplab7/StudentApp.java
+++ src/ooplab7/StudentApp.java
@@ -1,5 +1,7 @@
 package ooplab7;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
     // create object as student
@@ -15,7 +17,25 @@ public class StudentApp {
             "ML221");
     showData (s1);
     showData(s2);
+
+    Student s3 = new Student();
+    s3 = inputData(s3);
     }//m
+
+    private static Student inputData(Student s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter student info: ");
+        System.out.print("Enter student ID: ");
+        s.setId(scanner.nextLine());                     //อ่า่านค่า id เข้ามา
+        System.out.print("Enter student name: ");
+        s.setName(scanner.nextLine());
+        System.out.print("Enter student major: ");
+        s.setMajor(scanner.nextLine());
+        System.out.print("Enter student group: ");
+        s.setGroup(scanner.nextLine());
+        return s;
+
+    }//inputdata
 
     private static void showData(Student s) {
         System.out.println("Student Infoo: ");
