a09a52b4ec326f7b8481ae849181af63b771b151
Wanchanok <leejeebo13@gmail.com>
2018-01-22 14:26:29 +0700

    Student

---
+++ src/ooplab7/Student.java
@@ -0,0 +1,57 @@
+package ooplab7;
+//design class
+//taking abbut student?
+//1. student id
+//2. name
+//3. major
+//4. group
+
+public class Student {
+    private String id;
+    private String name;
+    private String major;
+    private String group;
+    private String n;
+
+    //constructor
+    //default
+    public Student () {}
+    public Student (String id,String n,String m,String g) {
+        this.id = id;
+        this.name = n;
+        this.major = m;
+        this.group = g;
+
+    }//constructor
+    //getter and setter methods
+    //id
+    public void setId(String id){
+        this.id = id;
+    }//set id
+    public String getId (){
+        return this.id;
+    }//getID
+
+    public void setName(String name){
+        this.name = name;
+    }//set name
+    public String getName () {
+        return  this.name;
+    }//getName
+
+    public void setMajor (String major){
+        this.major = major;
+    }//set major
+    public String getMajor () {
+        return  this.major;
+    }//getMajor
+
+    public void setGroup (String group){
+        this.group = group;
+    }//set group
+    public String getGroup () {
+        return  this.group;
+    }//getGroup
+
+
+}//class
---
+++ src/ooplab7/StudentApp.java
@@ -0,0 +1,28 @@
+package ooplab7;
+
+public class StudentApp {
+    public static void main(String[] args) {
+    // create object as student
+    Student s1 = new Student();
+    s1.setId("111111111111");
+    s1.setName("Boy SAIYAI");
+    s1.setMajor("Information system");
+    s1.setGroup("IS221");
+
+    Student s2 = new Student("222222222222",
+            "Girl SAIYAI",
+            "Maketing",
+            "ML221");
+    showData (s1);
+    showData(s2);
+    }//m
+
+    private static void showData(Student s) {
+        System.out.println("Student Infoo: ");
+        System.out.println("ID : " + s.getId());
+        System.out.println("Name : " + s.getName());
+        System.out.println("Major : " + s.getMajor());
+        System.out.println("Group : " + s.getGroup());
+
+    }//showdata
+}//c
