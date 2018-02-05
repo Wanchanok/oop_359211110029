158b62a438085ea904a1657796779f51affab745
LABCOM_6 <leejeebo13@gmail.com>
2018-02-05 15:14:35 +0700

    Car and preson App

--- src/ooplab9/PersonApp.java
+++ src/ooplab9/PersonApp.java
@@ -5,7 +5,7 @@ public class PersonApp {
 
         Person person = new Person("GB","029",
         new Address("123","Nakhon", "80000"),
-        new Job("student",10000));
+        new Job("studeet",10000));
 
         System.out.println(person.toString());
 
